f1299cf56e44def98d213cf6c2f9c9a4243384c1
태양 김 <achieve_sun@naver.com>
2018-11-20 12:45:54 +0900

    #ifdef #undef

--- Chapter_3/define.cpp
+++ Chapter_3/define.cpp
@@ -1,5 +1,28 @@
-//매크로 함수 미묘한 문제점
+//#ifdef #endif 사용법
 #if 1
+#include<stdio.h>
+#define DEBUG
+
+#ifdef DEBUG
+//#undef DEBUG
+void f(int i) {
+	printf("%d\n", i);
+}
+#endif
+
+void main() {
+	int i = 2, j = 3, k = 4;
+	k = i * j * k;
+#ifdef DEBUG
+	f(k);
+#endif
+	printf("%d, %d, %d\n", i, j, k);
+}
+
+#endif
+
+//매크로 함수 미묘한 문제점
+#if 0
 #include<stdio.h>
 #include "macro.h"
 #include "b.cpp"
@@ -10,7 +33,9 @@ extern int f(int i);
 void main() {
 	int i;
 	i = f(2);
-	PRINT(i);
+	while (1) {
+		PRINT(i);
+	}
 }
 
 #endif
